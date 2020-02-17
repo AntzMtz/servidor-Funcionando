b7dbb56bb6ae314c283128a562a47409e2b70532
Antz Escorpion <escorpion2005@gmail.com>
2020-02-17 17:00:42 -0600

    lista blanca

--- src/controllers/rutas.js
+++ src/controllers/rutas.js
@@ -9,15 +9,15 @@ const items = [
 ]
 
 var listaBlanca = [
-    'http://128.0.0.1:8081/components/login/src/GeneQr/Envia.html',
-    'http://localhost:5000/products3'
+    'http://127.0.0.1:8081/components/login/src/GeneQr/Envia.html',
+    'http://localhost:5000/products'
 ]
 
-var Url="http://";
+var Url = "http://";
 
 //Funciones
 
-const prin = (req, res,next) => {
+const prin = (req, res, next) => {
     res.render('index', {
         title: "My Servidor"
     })
@@ -30,58 +30,65 @@ const produ = (req, res, next) => {
     })
 }
 
-const rut=(req, res, next) => {
+const rut = (req, res, next) => {
     const nuevo = req.body.newItem
-    Url= req.body.newUrl
+    Url = req.body.newUrl
     next();
 }
+var corsOptions1;
+var corsOptions = function(req, callback, res) {
 
-var corsOptions = function(req, callback){
-    var corsOptions1;
     if (listaBlanca.indexOf(Url) !== -1) {
-        corsOptions1 = { origin: true } 
-        console.log("res: "+corsOptions1.origin);
-          callback(null, corsOptions1)
-      } else {
-        corsOptions1 = { origin: false } 
-        console.log("res: "+corsOptions1.origin);
-          callback(null, corsOptions1)
+        corsOptions1 = { origin: true }
+        console.log("res: " + corsOptions1.origin);
+        callback(null, corsOptions1)
+    } else {
+        corsOptions1 = { origin: false }
+        console.log("res: " + corsOptions1.origin);
+
+        callback(new Error(corsOptions1.origin))
     }
-     
-    
+
+
 }
 
 // var corsOptions =
-    //  {
-    // origin: function(origin, callback) {
-    //     if (listaBlanca.indexOf(Url) > -1) {
-    //         // console.log("ER:"+listaBlanca.indexOf(Url))
-    //         callback(null, true)
-    //     } else {
-    //         console.log("ER1"+Url);
-    //         //  Error="NO SE PUEDE POR CORS";  
-    //         // callback(Error)
-    //         // var nombre = ('Por favor ingresa tu nombre.')
-    //         callback(new Error("mojj"))
-    //     }
-        
-    // }
+//  {
+// origin: function(origin, callback) {
+//     if (listaBlanca.indexOf(Url) > -1) {
+//         // console.log("ER:"+listaBlanca.indexOf(Url))
+//         callback(null, true)
+//     } else {
+//         console.log("ER1"+Url);
+//         //  Error="NO SE PUEDE POR CORS";  
+//         // callback(Error)
+//         // var nombre = ('Por favor ingresa tu nombre.')
+//         callback(new Error("mojj"))
+//     }
+
+// }
 // }
 
 const addprodu = (req, res, next) => {
-    console.log("Estatus: "+corsOptions);
-        
-    const nuevo = req.body.newItem
-    const Url = req.body.newUrl
-    var URLSUB=Url.substring(Url.length - 9,Url.length);
-    items.push({
-        id: items.length + 1,
-        name: nuevo
-    });
-    console.log("Utre:"+URLSUB);
+    console.log("Estatus: " + corsOptions1.origin);
+    if (corsOptions1.origin == false) {
+        // res.json([{
+        //     items: "No tienes acceso al servidor"
+        // }])
+    } else {
+
+        const nuevo = req.body.newItem
+        const Url = req.body.newUrl
+        var URLSUB = Url.substring(Url.length - 9, Url.length);
+        items.push({
+            id: items.length + 1,
+            name: nuevo
+        });
+        console.log("Utre:" + URLSUB);
         res.json([{
             items: items
         }])
+    }
 }
 
 const getprodu = (req, res, next) => {
@@ -94,6 +101,6 @@ module.exports = {
     addprodu,
     getprodu,
     rut,
- corsOptions 
+    corsOptions
 
 }
